b7ceb70a7065ef1014512c77433644307d7bd8d1
home again <1@ldxy.edu.gr>
2018-01-25 18:30:26 +0800

    Update index.php

--- index.php
+++ index.php
@@ -22,6 +22,7 @@ class wechatCallbackapiTest
     public function tuling($postObj)
     {
         $key = "dd9ea2173ef44d29b1ad729346639c46";
+        $content = $postObj->Content;
         $re = json_decode(file_get_contents('http://www.tuling123.com/openapi/api?key='.$key.'&info='.$content.'&useid='.$fromUsername),true);
         $code = $re['code'];
         switch ($code){
