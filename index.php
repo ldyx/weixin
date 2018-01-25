e139ab43eb08a026e2027e42ec5ffdfa482fd1e7
home again <1@ldxy.edu.gr>
2018-01-25 11:48:46 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,25 +6,6 @@ $wechatObj->responseMsg();
 
 class wechatCallbackapiTest
 {
-    public function checkSignature()
-    {
-        $signature = $_GET["signature"];
-        $timestamp = $_GET["timestamp"];
-        $nonce = $_GET["nonce"];
-
-        $token = TOKEN;
-        $tmpArr = array($token, $timestamp, $nonce);
-        sort($tmpArr, SORT_STRING);
-        $tmpStr = implode( $tmpArr );
-        $tmpStr = sha1( $tmpStr );
-
-        if( $tmpStr == $signature ){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
     public function responseMsg()
     {
         $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
