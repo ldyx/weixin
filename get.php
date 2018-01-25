978b23148449ed45c9678ec4890f9cd5401e1af1
home again <1@ldxy.edu.gr>
2018-01-25 19:56:59 +0800

    Update get.php

--- get.php
+++ get.php
@@ -2,4 +2,4 @@
 $key = 'dd9ea2173ef44d29b1ad729346639c46';
 $re = json_decode(file_get_contents('http://www.tuling123.com/openapi/api?key='.$key.'&info=看新闻'),true);
 $list = $re['list'];
-echo $list[1]['article'];?>
+var_dump($list);
