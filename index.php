2a7fcfa5388109b0e20169c448b9406dce7f80ff
home again <1@ldxy.edu.gr>
2018-01-25 15:48:27 +0800

    Update index.php

--- index.php
+++ index.php
@@ -23,24 +23,39 @@ class wechatCallbackapiTest
     
     public function tuling($postObj)
     {
-            $fromUsername = $postObj->FromUserName;
-            $toUsername = $postObj->ToUserName;
-            $keyword = trim($postObj->Content);
-            $time = time();
-            $textTpl = "<xml>
-                        <ToUserName><![CDATA[%s]]></ToUserName>
-                        <FromUserName><![CDATA[%s]]></FromUserName>
-                        <CreateTime>%s</CreateTime>
-                        <MsgType><![CDATA[%s]]></MsgType>
-                        <Content><![CDATA[%s]]></Content>
-                        <FuncFlag>0</FuncFlag>
-                        </xml>";
         
-                $msgType = "text";
-                $contentStr = date("Y-m-d H:i:s",time());
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $keyword);
-                echo $resultStr;
-        
+        $fromUsername = $postObj->FromUserName;
+        $toUsername = $postObj->ToUserName;
+        $content = trim($postObj->Content);
+        $time = time();
+        $key = "dd9ea2173ef44d29b1ad729346639c46";
+        $re = json_decode(file_contents('http://www.tuling123.com/openapi/api?key='.$key.'&info='.$content.'&useid='.$fromUsername),true);
+        $code = $re['code'];
+        switch ($code){
+            case 100000:
+                $content = $re['text'];
+                break;
+            case 200000:
+                $content = $re['text'].$re['url'];
+                break;
+            case 40004:
+                $content= '今天累了，明天接着聊啊';
+                break;
+            default:
+                $content = $re['text'];
+        }
+        $textTpl = "<xml>
+                    <ToUserName><![CDATA[%s]]></ToUserName>
+                    <FromUserName><![CDATA[%s]]></FromUserName>
+                    <CreateTime>%s</CreateTime>
+                    <MsgType><![CDATA[%s]]></MsgType>
+                    <Content><![CDATA[%s]]></Content>
+                    <FuncFlag>0</FuncFlag>
+                    </xml>";
+         $msgType = "text";
+         $contentStr = date("Y-m-d H:i:s",time());
+         $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $keyword);
+         echo $resultStr;
     }
 }
 ?>
