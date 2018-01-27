f3e358b41fc9e489941186e33a5f2f5a4594ae8b
home again <1@ldxy.edu.gr>
2018-01-27 14:02:44 +0800

    Update index.php

--- index.php
+++ index.php
@@ -38,69 +38,60 @@ class wechatCallbackapiTest
 	Conf::setAppInfo($appid, $secretId, $secretKey, $userid,conf::API_YOUTU_END_POINT);
         //人脸检测接口调用
         $uploadRet = YouTu::fuzzydetecturl("$pic",1);
-
-if ($uploadRet['fuzzy_confidence']<=0.3){
-$uploadRet = YouTu::imagetagurl("$pic");
-$tags = $uploadRet['tags'];
-$tagsNum = count($tags);
-
-$maxConfidence = 0;
-for ($i=0;$i<$tagsNum;$i++)
-{
-	$tagConfidence = $tags[$i]['tag_confidence'];
-	if ($maxConfidence <= $tagConfidence)
+	if ($uploadRet['fuzzy_confidence'] <= 0.3)
 	{
-		$tagName = $tags[$i]['tag_name'];
-		$maxConfidence = $tagConfidence;
-	}
-}
-switch ($tagName)
-{
-	case "女孩":
-		$uploadRet = YouTu::detectfaceurl("$pic",1);
-		$gender = "女性";
-
-	case "男孩":
-		$uploadRet = YouTu::detectfaceurl("$pic",1);
-		if ($gender = "女性"){
-		
-		}else{$gender = "男性";}
-		$age = $uploadRet['face'][0]['age'];
-		$beauty = $uploadRet['face'][0]['beauty'];
-		$expression = $uploadRet['face'][0]['expression'];
-		$glasses = $uploadRet['face'][0]['glasses'];
-		
-		if ($glasses == "0"){
-			$glasses = "没戴眼镜";
-		}elseif($glasses == "1"){
-			$glasses = "戴了眼镜";
-		}else{$glasses = "戴了墨镜";}
-		$content ="检测结果如下：\n年龄：".$age."\n性别：".$gender."\n颜值：".$beauty."\n微笑程度：".$expression."\n".$glasses;
-		$this -> text($postObj,$content);
-		break;
-	case "文本":
-		$uploadRet = YouTu::generalocrurl("$pic", $seq = '');
-		var_dump($uploadRet);
-		$items = $uploadRet['items'];
-		$itemsNum = count($items);
-		$content = "";
-		for ($i=0;$i<$itemsNum;$i++)
+	$uploadRet = YouTu::imagetagurl("$pic");
+	$tags = $uploadRet['tags'];
+	$tagsNum = count($tags);
+	$maxConfidence = 0;
+	for ($i=0;$i<$tagsNum;$i++)
+	{
+		$tagConfidence = $tags[$i]['tag_confidence'];
+		if ($maxConfidence <= $tagConfidence)
 		{
-			$content = $content."\n".$items[$i]['itemstring'];
+			$tagName = $tags[$i]['tag_name'];
+			$maxConfidence = $tagConfidence;
 		}
-		$content = $pic;
-		$this -> text($postObj,$content);
-		break;
-	default:
-		$content = $tagName;
-		
-		$this -> text($postObj,$content);
-}}else
-{
-	//var_dump($uploadRet);
-	$a = 0.8;
-	var_dump($a);
-	echo "亲，你提供的图片太模糊了，我看不清啊";
+	}
+	switch ($tagName)
+	{
+		case "女孩":
+			$gender = "女性";
+		case "男孩":
+			if ($gender == "女性"){
+				
+			}else{$gender = "男性";}
+			$uploadRet = YouTu::detectfaceurl("$pic",1);
+			$age = $uploadRet['face'][0]['age'];
+			$beauty = $uploadRet['face'][0]['beauty'];
+			$expression = $uploadRet['face'][0]['expression'];
+			$glasses = $uploadRet['face'][0]['glasses'];
+			if ($glasses == "0"){
+				$glasses = "没戴眼镜";
+			}elseif($glasses == "1"){
+				$glasses = "戴了眼镜";
+			}else{$glasses = "戴了墨镜";}
+			$content = "检测到以下结果\n"."年龄：".$age."\n颜值：".$beauty."\n性别：".$gender."\n微笑程度：".$expression."\n".$glasses;
+			$this -> text($postObj,$content);
+			break;
+		case "文本":
+			$uploadRet = YouTu::generalocrurl("$pic", $seq = '');
+			$items = $uploadRet['items'];
+			$itemsNum = count($items);
+			$content = "";
+			for ($i=0;$i<$itemsNum;$i++)
+			{
+				$content = $content."\n".$items[$i]['itemstring'];
+			}
+			$this -> text($postObj,$content);
+			break;
+		default:
+			$content = $tagName;
+			$this -> text($postObj,$content);
+	}
+}else{
+	$content = "亲，你的图片太模糊了，看不清啊";
+	echo $content;
 }
     }
     
